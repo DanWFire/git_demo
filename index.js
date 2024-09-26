1a8946b39662c989aecb9908f921d2b4ae4cafd9
DanWFire <heluxue3737@gmail.com>
2024-09-26 20:02:06 +1000

    feat:add main page

--- index.js
+++ index.js
@@ -2,6 +2,19 @@ const express = require('express')
 
 const app = express()
 
+app.get('/',function(req,res){
+    res.send('Hello world')
+})
+
 app.listen(8080,function(){
     console.log('Listening at port 8080')
 })
+
+
+//git init 
+//
+//git add .
+//git commit -m '##'
+//git push
+
+//git switch -c feat-001
